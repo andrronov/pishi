34df8bc456dda4f91193b7f4c23eb208a1dd7cb6
andrronov <andrew270504cska@gmail.com>
2024-02-01 14:09:46 +0300

    chat design + sending msgs done -> realtime

---
+++ layouts/chat.vue
@@ -0,0 +1,26 @@
+<template>
+   <div :class="{'dark': lightMode}">
+      <div class="bg-black text-white dark:bg-white ">
+         <div class="min-h-full">
+            <div class="grid grid-cols-12 mx-auto sm:px-6 gap-3 lg:max-w-7xl lg:px-8 lg:gap-5">
+               <!-- left sidebar -->
+               <div class="md:block col-span-2 xs:col-span-1 sm:col-span-2">
+                  <div class="sticky top-0">
+                     <SidebarLeft />
+                  </div>
+               </div>
+ 
+               <!-- main content -->
+               <main class="col-span-10 xs:col-span-11 md:col-span-10">
+                  <slot />
+               </main>
+ 
+            </div>
+         </div>
+      </div>
+   </div>
+ </template>
+
+<script setup>
+const lightMode = ref(false)
+</script>
--- pages/messages/chat/[id].vue
+++ pages/messages/chat/[id].vue
@@ -1,13 +1,62 @@
 <template>
-  chat
+  <div class="w-full flex flex-col items-center h-screen">
+    <div class="bg-gray-800 dark:bg-gray-200 w-full h-full overflow-y-scroll" id="chat">
+      <div v-if="messages" class="flex flex-col w-full justify-around px-4 py-2 gap-4">
+        <div v-for="(msg, index) in messages" :key="index" :class="msg.author == session.data.session.user.id ? 'bg-white text-black dark:bg-black dark:text-white self-end' : 'bg-black text-white dark:bg-white dark:text-black self-start'" class="flex flex-row px-1 py-1 justify-between min-h-10 border-2 border-gray-500 min-w-min max-w-max gap-3" id="myMessage">
+          <p>{{ msg.text }}</p>
+          <p class="text-gray-500 text-xs self-end">{{ msg.created_at.hour }}:{{ msg.created_at.minutes }}</p>
+        </div>
+      </div>
+    </div>
+    <div class="flex flex-row items-center w-full">
+      <input v-model="inputMessage" type="text" class="p-1 text-black dark:text-white dark:bg-black w-full">
+      <button @click="sendMessage" class="py-1 w-24 hover:bg-gray-500">Send</button>
+    </div>
+  </div>
 </template>
 
-<script>
-export default {
+<script setup>
+definePageMeta({
+  layout: 'chat'
+})
 
+// VARIABLES
+const supabase = useSupabaseClient()
+const session = await supabase.auth.getSession();
+const route = useRoute()
+
+
+const messages = ref([])
+const inputMessage = ref(null)
+// -----------------------------
+
+async function loadMessages(){
+  const {data, error} = await supabase.from('chat_messages').select().eq('chat_id', route.params.id)
+  if(!error){
+    data.map((item) => {
+      const itemTime = new Date(item.created_at)
+      const hour = itemTime.getHours()
+      const minutes = itemTime.getMinutes()
+      return item.created_at = {hour, minutes}
+    })
+    messages.value.push(...data)
+    console.log(messages.value);
+  }
+  console.log(error);
 }
+
+async function sendMessage(){
+  if(inputMessage.value){
+    const {data, error} = await supabase.from('chat_messages').insert({chat_id: route.params.id, author: session.data.session.user.id, text: inputMessage.value})
+    if(!error){
+      inputMessage.value = null
+    }
+  } else {
+    console.log('xxx');
+  }
+}
+
+watchEffect(() => {
+  loadMessages()
+})
 </script>
-
-<style>
-
-</style>
